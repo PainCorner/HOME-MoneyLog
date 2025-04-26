fd227b67310f91b9c31209c401d932baa0a2bae8
PainCorner <141883811+PainCorner@users.noreply.github.com>
2025-04-26 19:29:23 +0900

    アラートを出力する

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World App',
       home: Scaffold(
-        appBar: AppBar(title: const Text('ボタン'),
-          backgroundColor: Colors.blue,
-
+        backgroundColor: Colors.blue[50],
+        appBar: AppBar(
+          title: Text(
+            "支出管理",
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
         ),
         body: Center(
-          child: ElevatedButton(
-            onPressed: (){
-              print("ボタンが押されました");
-            },
-            child: const Text("押してみて？"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("成功しました!", style: TextStyle(fontSize: 40)),
+              SizedBox(height: 30),
+              Builder( // ここでBuilder使うのがポイント！
+                builder: (context) => ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text("お知らせ"),
+                          content: Text("アラート成功！！"),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text("OK"),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    shape: StadiumBorder(),
+                  ),
+                  child: Text("続けて管理する", style: TextStyle(fontSize: 20, color: Colors.white)),
+                ),
+              ),
+            ],
           ),
         ),
       ),
